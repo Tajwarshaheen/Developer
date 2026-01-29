14741d94a709db575bc610c74bdc7df1831c3aa6
Tajwar <wazirsaqlain1011@gmail.com>
2026-01-29 04:26:00 -0800

    sdfdsf

---
+++ my-project/src/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Header() {
+  return (
+   <>
+   <div className='text-lime-300 bg-cyan-800 font-bold'>Hello world</div>
+   </>
+  )
+}
+
+export default Header
